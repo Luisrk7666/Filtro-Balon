a3b6402ba9c575e5d0529e675c51e0e27b46c51b
Luisrk7666 <127268823+Luisrk7666@users.noreply.github.com>
2023-10-11 20:06:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX=0;
+noseY=0;
+function preload() {
+balon=loadImage("balon.png")
+}
+function setup() {
+canvas=createCanvas(300,300)
+canvas.center();
+video=createCapture(VIDEO)
+video.hide()
+poseNet=ml5.poseNet(video,modelLoaded)
+poseNet.on("pose",gotResults)
+
+}
+function modelLoaded(){
+console.log ("poseNet.inicializado")
+
+}
+function gotResults(results){
+    console.log (results)
+    noseX=results[0].pose.nose.x-250;
+    noseY=results[0].pose.nose.y-250;
+console.log (noseX,noseY);
+}
+
+function draw() {
+image(video,0,0,300,300);
+image(balon,noseX,noseY,50,50);
+}
+function take_snapshot() {
+ save("MyPhoto.png")   
+}
